24cfad71e1f725ba6c0ee76439d75c9e4e023f07
Chandan Kumar <chandankumarchandankumar38851@gmail.com>
2026-04-04 02:13:35 +0530

    setup auth functionality

---
+++ src/appwrite/auth.js
@@ -0,0 +1,61 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const user = await this.account.create({
+        userId: ID.unique(),
+        email: email,
+        password: password,
+        name: name,
+      });
+      if (user) {
+        return this.login({ email, password });
+      } else {
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const result = await this.account.createEmailPasswordSession({
+        email,
+        password,
+      });
+      //   return result
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error ", error);
+    }
+    return null;
+  }
+  async logout(){
+    try {
+        await this.account.deleteSessions()
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error ", error);
+
+        
+    }
+  }
+}
+const authService = new AuthService();
+export default authService;
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+// const { configureStore } = require("@reduxjs/toolkit");
+
+import { configureStore } from "@reduxjs/toolkit"
+
+const store=configureStore({
+    reducer:{}
+})
+
+export default store
+
